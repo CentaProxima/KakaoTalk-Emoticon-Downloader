2792e60eac20da84268cc2b898ff4b0357a1267c
CentaProxima <dsuctf2022@gmail.com>
2022-06-14 02:44:23 +0900

    카카오톡 이모티콘 다운로드

--- src/main/java/me/proxima/kakao/emoticon/Downloader.java
+++ src/main/java/me/proxima/kakao/emoticon/Downloader.java
@@ -1,21 +1,22 @@
 package me.proxima.kakao.emoticon;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.util.Scanner;
+import java.io.InputStreamReader;
 
 public class Downloader {
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter Path to Download: ");
-        String path_ = scanner.nextLine();
-        String path = path_.isEmpty() ? "." : path_;
+        String _path = scanner.readLine();
+        String path = _path.isEmpty() ? "." : _path;
 
         if(!path.equals(".")){
             File dir = new File(path);
             if(!dir.isDirectory()) dir.mkdirs();
         }
 
-        int threadCount = 10;
+        int threadCount = 6;
         int totalCount = 100000;
         int downloadPerThread = totalCount/threadCount; //전체 다운로드 갯수 / 생성 스레드 수
         for(int i=0; i<threadCount; i++){
